6c8f5b906df7b094d4da495fafca14b8afc3ab42
David Schlenk <david.schlenk@spanlink.com>
2014-05-22 17:08:47 -0500

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'david.schlenk@spanlink.com'
 license          'Apache 2.0'
 description      'Cookbook to ease using Jenkins for Chef Cookbook CI'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'java'
 depends 'jenkins'
