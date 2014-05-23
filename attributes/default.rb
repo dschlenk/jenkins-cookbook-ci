5c5b240bf43ee27a7bc76efb3a81d320e5e4567b
David Schlenk <david.schlenk@spanlink.com>
2014-05-23 15:48:24 -0500

    Add kitchen-openstack to the list of rvm gems.

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,10 @@ when 'rhel'
   default['rvm']['user_installs'] = [
     { 'user' => 'jenkins',
       'home' => '/var/lib/jenkins',
-      'default_ruby' => 'ruby-1.9.3'
+      'default_ruby' => 'ruby-1.9.3', 
+      'global_gems' => [
+        { 'name' => 'kitchen-openstack' }
+      ]
     }
   ]
 end
